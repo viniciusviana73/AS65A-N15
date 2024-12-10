e1e6af29757a661a0ee4c7ed36b9015729eb9325
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2024-12-09 22:50:21 -0300

    Create Product.js

---
+++ server/models/Product.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    isDonate: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    amount: {
+        type: Number,
+        required: true,
+        default: 0,
+        min: 0
+    },
+    distribuidorID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Distribuidor',
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Product', productSchema);
