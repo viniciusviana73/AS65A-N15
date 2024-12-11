32c7a5e63e756afaf424e189cdfb7e154f5c18a9
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2024-12-10 21:00:59 -0300

    admin login render

--- server/services/render.js
+++ server/services/render.js
@@ -1,3 +1,20 @@
+const Admin = require("../models/Admin");
+
 exports.indexRender = async (req, res) => {
     return res.render('index');
+}
+
+exports.adminLoginRender = async (req, res) => {
+    try {
+        const adminCount = await Admin.countDocuments();
+
+        if (adminCount === 0) {
+            return res.render("auth/signup");
+        }
+
+        return res.render("auth/signin");
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Houve um erro interno no servidor.", details: error });
+    }
 }
