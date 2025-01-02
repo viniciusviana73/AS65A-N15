f699bc6a56de52f91c79903e83c969cd295e7d2a
viniciusviana73 <54729152+viniciusviana73@users.noreply.github.com>
2025-01-01 22:57:24 -0300

    Update script.js
    - fillTable()

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,28 @@
-console.log("...");
+(async function fillTable() {
+    const tbody = document.querySelector('.product-table tbody');
+    tbody.innerHTML = '';
+
+    try {
+        const data = await fetch('/api/table', { method: 'GET', headers: { 'Content-Type': 'application/json', }, });
+        const response = await data.json();
+        console.log("response:");
+        console.log(response);
+
+        response.forEach(row => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                        <td>${row.name}</td>
+                        <td>${row.entrada}</td>
+                        <td>${row.doacao}</td>
+                        <td>${row.entregues}</td>
+                        <td>${row.estoque}</td>
+                        <td>${row.totalProdutos}</td>
+                    `;
+            tbody.appendChild(tr);
+        });
+
+    } catch (error) {
+        console.error(`Erro ao preencher a tabela: ${error.message}`);
+        console.error(error);
+    }
+})();
